11c46589c2e7573e71a6a74a0d5bf320845dac6e
Alexandre L'Erario <alerario@gmail.com>
2018-05-15 13:15:55 -0300

    adicionado script para inserir registros nas tabelas

---
+++ script/database/insert_records.sql
@@ -0,0 +1,10 @@
+-- script para inserir registros basicos no banco
+-- antes de cada conjunto de inserts o nome do responsavel deve ser informado
+
+
+--Responsavel: Alexandre L'Erario
+insert into teste values (1, 'Teste 1');
+insert into teste values (2, 'Teste dois');
+insert into teste values (3, 'Terceiro Teste');
+insert into teste values (4, 'Teste 1234');
+insert into teste values (5, 'Teste five');
