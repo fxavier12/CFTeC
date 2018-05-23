2df1075f547fe9849cd7881270fb2b607bfcaef2
Tiago Pagotto <pagotto@alunos.utfpr.edu.br>
2018-05-23 20:54:39 -0300

    adicionei inserts da tabela purpose

--- script/database/insert_records.sql
+++ script/database/insert_records.sql
@@ -8,3 +8,11 @@ insert into teste values (2, 'Teste dois');
 insert into teste values (3, 'Terceiro Teste');
 insert into teste values (4, 'Teste 1234');
 insert into teste values (5, 'Teste five');
+
+--Responsavel: Tiago Pagotto
+insert into purpose values (1, 'entrega', 'mensagem de engtrega');
+insert into purpose values (2, 'social', 'mensagem de cunho social');
+insert into purpose values (3, 'consulta', 'mensagem para consultar algo');
+insert into purpose values (4, 'ruido', 'mensagem emitida de forma automatica');
+insert into purpose values (5, 'ajuste', 'mensagem para ajustar algo');
+--falta inserts da tabela commUnit
