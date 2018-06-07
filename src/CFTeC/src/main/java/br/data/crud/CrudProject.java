0676bee3a91256816a8192d5bf8e461997699772
Diego Vilella Rodrigues <divilella@gmail.com>
2018-06-07 17:09:05 -0300

    first commit
    Iniciando a criação da tarefa escolhida, alguns problemas com a autenticação do tom cat impossibilita a continuidade do desenvolvimento da tarefa;

---
+++ src/CFTeC/src/main/java/br/data/crud/CrudProject.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.data.crud;
+
+import br.data.entity.Project_;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author alexandrelerario
+ */
+public class CrudProject extends AbstractCrud<br.data.entity.Project> {
+
+    private EntityManager em;
+
+    private static final String PU = EMNames.EMN1;
+
+    public CrudProject() {
+        super(br.data.entity.Project.class);
+    }
+
+    public List<br.data.entity.Project> SelectByNome(String nome) {
+        List<br.data.entity.Project> lista;
+        try { 
+            lista= getEntityManager().createNamedQuery("Project.findByNome").setParameter("nome", "%" + nome.toUpperCase() + "%").getResultList();
+            return lista;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        if (em == null) {
+            em = Persistence.createEntityManagerFactory(PU).createEntityManager();
+        }
+        return em;
+    }
+
+}
