0f5a51c849ee3120d5538e314497e268c469d310
Gabriel Tobias <gabriel-tobias02@hotmail.com>
2018-06-07 10:41:34 -0300

    Update insert_records.sql

--- script/database/insert_records.sql
+++ script/database/insert_records.sql
@@ -21,3 +21,28 @@ insert into communit values (2, 2, 2);
 insert into communit values (3, 3, 1);
 insert into communit values (4, 3, 4);
 insert into communit values (5, 5, 5);
+
+-- Responsavel: Gabriel Tobias 
+insert into state values (1, 'Parana');
+insert into state values (2, 'Rio Grande do Sul');
+insert into state values (3, 'Amazonas');
+insert into state values (4, 'Minas Gerais');
+insert into state values (5, 'Sao Paulo');
+
+insert into city values (1, 'Cornelio Procopio', 1);
+insert into city values (2, 'Porto Alegre', 2);
+insert into city values (3, 'Campinas', 5);
+insert into city values (4, 'Manaus', 3);
+insert into city values (5, 'Belo Horizonte', 4);
+
+insert into type values (1, 'Rua');
+insert into type values (2, 'Avenida');
+insert into type values (3, 'Praça');
+insert into type values (4, 'Viaduto');
+insert into type values (5, 'Rodovia');
+
+insert into postalcode values (1, '00152-000', 4, 3);
+insert into postalcode values (2, '93215-111', 2, 4);
+insert into postalcode values (3, '15987-258', 1, 1);
+insert into postalcode values (4, '02784-333', 5, 5);
+insert into postalcode values (5, '87410-045', 3, 2);
