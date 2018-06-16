6c1c80fb7f87a840421a042bba730b48d6c42a4e
Yoshikazu Sazaka <ysazaka@gmail.com>
2018-06-16 10:19:10 -0300

    Adicionado a entidade Message com o atributo codigo por ser relacionado com Evaluation

--- src/CFTeC/src/main/java/br/data/entity/Evaluation.java
+++ src/CFTeC/src/main/java/br/data/entity/Evaluation.java
@@ -6,6 +6,7 @@
 package br.data.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -27,8 +28,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Evaluation.findAll", query = "SELECT t FROM Evaluation t")
-    , @NamedQuery(name = "Evaluation.findByCodigo", query = "SELECT t FROM Evaluation t WHERE t.codigo = :codigo")
-    , @NamedQuery(name = "City.findByNome", query = "SELECT t FROM City t WHERE upper(t.nome) like :nome")})
+    , @NamedQuery(name = "Evaluation.findByCodigo", query = "SELECT t FROM Evaluation t WHERE t.codigo = :codigo")})
 public class Evaluation implements Serializable{
     private static final long serialVersionUID = 1L;
     
@@ -36,16 +36,56 @@ public class Evaluation implements Serializable{
     @NotNull
     @Column(name = "codigo")
     private Integer codigo;
-//    @JoinColumn(name = "codigo_collaborator", referencedColumnName = "codigo")
-//    @ManyToOne(optional = false)
-//    private Collaborator codigo_collaborator;
-//    @JoinColumn(name = "codigo_message", referencedColumnName = "codigo")
-//    @ManyToOne(optional = false)
-//    private Message codigo_message;
-//    private Date data;
+    @JoinColumn(name = "codigo_collaborator", referencedColumnName = "codigo")
+    @ManyToOne(optional = false)
+    private Collaborator codigo_collaborator;
+    @JoinColumn(name = "codigo_message", referencedColumnName = "codigo")
+    private Message codigo_message;
+    private Date data;
     private float value;
 
     // Getters and Setters
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Collaborator getCodigo_collaborator() {
+        return codigo_collaborator;
+    }
+
+    public void setCodigo_collaborator(Collaborator codigo_collaborator) {
+        this.codigo_collaborator = codigo_collaborator;
+    }
+
+    public Message getCodigo_message() {
+        return codigo_message;
+    }
+
+    public void setCodigo_message(Message codigo_message) {
+        this.codigo_message = codigo_message;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+    
     
     // Constructor
     public Evaluation() {}
